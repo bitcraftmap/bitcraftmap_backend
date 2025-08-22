e5f5583783c97a207048fa8ee18b66a6ff24bedd
bitcraftmap <bitcraftmap@emailproxy.org>
2025-08-22 14:35:56 +0200

    Initial commit

---
+++ burn_docker.sh
@@ -0,0 +1,8 @@
+# stop and remove all containers
+docker rm -f $(docker ps -aq)
+
+# remove all volumes
+docker volume rm -f $(docker volume ls -q)
+
+# wipe images and networks
+docker system prune -a --volumes -f
