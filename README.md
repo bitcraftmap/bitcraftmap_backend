7b11c9f6e18917a6ae29e92f837456043c22ae92
bitcraftmap <bitcraftmap@emailproxy.org>
2025-08-24 14:31:13 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Backend for Bitcraft Map
+
+- `git submodule add https://github.com/vis-eyth/bitcraft-bindings.git stdb_bindings`
--- stdb_explorer/src/main.rs
+++ stdb_explorer/src/main.rs
@@ -1,10 +1,5 @@
 mod module_bindings { pub mod region; }
 
-use module_bindings::region::DbConnection as RegionDb;
-use module_bindings::region::*;
-use spacetimedb_sdk::{Event, DbContext};
-use std::any::type_name_of_val;
-
 #[tokio::main]
 async fn main() {
     dotenvy::dotenv().ok();
